9878fd5e9dc8d6473ee7b38773c3adbffe89b0c3
shwanoff <adm.shwan@gmail.com>
2025-03-11 00:15:20 +0100

    Created dummy endpoints

---
+++ OrderProcessing.API/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderProcessing.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class OrdersController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult CreateOrder()
+		{
+			// TODO: Implement order creation
+			return Ok("CreateOrder endpoint");
+		}
+
+		[HttpGet("{orderNumber}")]
+		public IActionResult GetOrder(string orderNumber)
+		{
+			// TODO: Implement order retrieval
+			return Ok($"GetOrder endpoint for order number: {orderNumber}");
+		}
+	}
+}
